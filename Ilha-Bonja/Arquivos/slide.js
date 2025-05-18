1c2e17d5beb184b80b1e0f56cc0146eb2fb011df
Silas Álefe <116173586+SilasAlefe@users.noreply.github.com>
2025-05-18 16:02:52 -0300

    Autalização-18-05-2025-16:02

--- Ilha-Bonja/Arquivos/slide.js
+++ Ilha-Bonja/Arquivos/slide.js
@@ -1,14 +1,19 @@
 
-document.querySelector('#radio1').checked = true
+cont = 1
+
+window.document.querySelector('#radio1').checked = true
+
 setInterval(passagemauto, 2500)
 
 function passagemauto(){
-    cont = 1
 
-    if (querySelector(`#radio`+cont).checked = true){
-        cont++
-    } else{
-        querySelector(`#radio`+cont).checked = true
+    if (cont<3){
+        if (document.querySelector('#radio'+cont).checked == true){
+            cont++
+        } else {
+            document.querySelector('#radio'+cont).checked = true
+        }
+    }else{
         cont = 1
     }
 }
